d5503af965b9726951a61411343071f96d6de072
xywms <32957647+xywms@users.noreply.github.com>
2022-03-08 16:37:33 +0800

    Add files via upload

---
+++ 反转部分链表_递归.cpp
@@ -0,0 +1,43 @@
+/**
+ * struct ListNode {
+ *	int val;
+ *	struct ListNode *next;
+ * };
+ */
+
+class Solution {
+public:
+    /**
+     * 
+     * @param head ListNode类 
+     * @param m int整型 
+     * @param n int整型 
+     * @return ListNode类
+     */
+    ListNode *successor = nullptr; // 后驱节点
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        // write code here
+        //递归
+        if(m == 1) {
+            return reverseN(head,n);
+        }
+        // 前进到反转的起点触发 base case
+        head->next = reverseBetween(head->next, m - 1, n - 1);
+        return head;
+    }
+    // 反转以 head 为起点的 n 个节点，返回新的头结点
+    ListNode* reverseN(ListNode* head, int n) {
+        if(n == 1) {
+            // 记录第 n + 1 个节点
+            successor = head->next;
+            return head;
+        } 
+        // 以 head->next 为起点，需要反转前 n - 1 个节点
+        ListNode* last = reverseN(head->next,n-1);
+        head->next->next=head;
+         // 让反转之后的 head 节点和后面的节点连起来
+        head->next = successor;
+        //last即为反转之后的头节点
+        return last;
+    }
+};
